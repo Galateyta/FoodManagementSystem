0483058825e54abca58816f2c3d4364a8165d967
Vahagn <sargsyanvahag6@gmail.com>
2023-09-17 13:35:20 +0400

    Deleted some method for sending email

--- Authentication/Authentication.h
+++ Authentication/Authentication.h
@@ -10,7 +10,6 @@ class Authentication {
     void profile();
     void registration();
     bool lettersAndNumbers(std::string& password);
-    //size_t read_callback(void *ptr, size_t size, size_t nmemb, void *stream);
     std::string generateID();
     std::string username, password;
     int maxOilCount;
